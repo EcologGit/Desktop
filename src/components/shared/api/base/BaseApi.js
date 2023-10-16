934019d3564b7106d8f55c1c71c5e7b930265072
merf-ai <nik.bolshakov.14@bk.ru>
2023-10-17 00:40:17 +0300

    изменил адрес

--- src/components/shared/api/base/BaseApi.js
+++ src/components/shared/api/base/BaseApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://127.0.0.1:8000/django_api";
+const url = "http://81.163.30.36/django_api";
 
 export const baseApi = axios.create({
   baseURL: url,
