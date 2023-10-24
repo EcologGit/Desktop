fec74b062d13b34692b4429e6421a934d3bda6ca
merf-ai <nik.bolshakov.14@bk.ru>
2023-10-24 20:43:57 +0300

    Модалка + собранные отходы в создании репорта

--- src/components/createReportPage/newReportComponent.vue
+++ src/components/createReportPage/newReportComponent.vue
@@ -104,11 +104,11 @@
           />
         </button>
         <div>
-        <p class="photo-label" style="font-size: medium" v-if="photo">
-          {{ photo }}
-          <span @click="deleteFile" style="cursor: pointer">&cross;</span>
-        </p>
-      </div>
+          <p class="photo-label" style="font-size: medium" v-if="photo">
+            {{ photo }}
+            <span @click="deleteFile" style="cursor: pointer">&cross;</span>
+          </p>
+        </div>
       </div>
       <div class="desc-newReport">
         <p class="desc-newReport-label">Отчет*</p>
@@ -164,86 +164,26 @@
           </div>
         </div>
       </div>
-      <div class="newReport-wrapping">
+      <div class="newReport-wrapping" style="padding-top: 10px">
         <div class="star-newReport"></div>
         <div class="star-newReport-points">
           <div class="list-points">
-            <div class="point">
+            <p class="photo-label">Собранные отходы</p>
+            <select style="margin-top: 20px; margin-bottom: 20px" multiple v-model='wasteSelectValue'>
+              <option v-for='waste in wasteTypes' :key='waste.pk' :value='waste.pk' size="4">{{waste.name}}</option>
+            </select>
+            <div v-for='item in wasteSelectValue' :key='item' class="point">
               <div class="rate">
                 <img
-                  src="../../assets/imgs/plastic_trash_type.png"
+                  :src="getWasteSrcImageByPk(item)"
                   alt=""
                   class="cirlce-img"
                 />
-                Пластик
+                {{getWasteByPk(item)?.name}}
               </div>
               <div class="input-number-wrapping">
-                <input class="input-number" min="0" type="number" />кг
-              </div>
-            </div>
-            <div class="point">
-              <div class="rate">
-                <img
-                  src="../../assets/imgs/glass_trash_type.png"
-                  alt=""
-                  class="cirlce-img"
-                />
-                Стекло
-              </div>
-              <div class="input-number-wrapping">
-                <input class="input-number" min="0" type="number" />кг
-              </div>
-            </div>
-            <div class="point">
-              <div class="rate">
-                <img
-                  src="../../assets/imgs/batteries_trash_type.png"
-                  alt=""
-                  class="cirlce-img"
-                />
-                Батарейки
-              </div>
-              <div class="input-number-wrapping">
-                <input class="input-number" min="0" type="number" />кг
-              </div>
-            </div>
-            <div class="point">
-              <div class="rate">
-                <img
-                  src="../../assets/imgs/light_bulbs_trash_type.png"
-                  alt=""
-                  class="cirlce-img"
-                />
-                Лампочка
-              </div>
-              <div class="input-number-wrapping">
-                <input class="input-number" min="0" type="number" />кг
-              </div>
-            </div>
-            <div class="point">
-              <div class="rate">
-                <img
-                  src="../../assets/imgs/paper_trash_type.png"
-                  alt=""
-                  class="cirlce-img"
-                />
-                Макулатура
-              </div>
-              <div class="input-number-wrapping">
-                <input class="input-number" min="0" type="number" />кг
-              </div>
-            </div>
-            <div class="point">
-              <div class="rate">
-                <img
-                  src="../../assets/imgs/metal_trash_type.png"
-                  alt=""
-                  class="cirlce-img"
-                />
-                Металл
-              </div>
-              <div class="input-number-wrapping">
-                <input class="input-number" min="0" type="number" />кг
+                <input class="input-number" min="0" type="number" :key='item' @change='(event) => changeWasteInputHandler(event, item)'/>кг
+                <span @click="deleteWaste(item)" style="cursor: pointer">&#10006;</span>
               </div>
             </div>
           </div>
@@ -251,7 +191,7 @@
       </div>
       <div class="required-fields">* – поля, обязательные к заполнению</div>
       <div class="buttons-newReport">
-        <button class="newReport-publish">
+        <button class="newReport-publish" @click='openModal'>
           <img
             class="a-img"
             width="18"
@@ -273,12 +213,26 @@
       </div>
     </section>
   </main>
+  <div class='modal-background' v-if='modalIsOpen'><div class='modal-content' style='width: 30%; height: 30%'>
+    <div style='width:100%'>
+    <div style='display: flex; width: 100%; justify-content: right'>
+    <span style="cursor: pointer; margin-right: 20px; font-size: xx-large;">&#10006;</span>
+  </div>
+    <p style='display: flex; justify-content: center; font-size: x-large'>Вы уверены?</p>
+  </div>
+    <div class='modal-buttons-new-report-block'>
+    <button class='modal-button-new-report'>Да</button>
+    <button class='modal-button-new-report' @click='closeModal'>Отмена</button>
+    </div>
+  </div></div>
 </template>
 
 <script>
 import { reportUrls } from "@/components/apiUrls/report/reportUrls.js";
 import { baseApi } from "@/components/shared/api/base/BaseApi.js";
 import { ref } from "vue";
+import { wasteDictImagesDict } from "..//..//consts//waste//wasteImages.js";
+import './style.css';
 
 export default {
   setup() {
@@ -287,7 +241,13 @@ export default {
     const allPlaces = ref([]);
     const allSortPoints = ref([]);
     const fileInput = ref(null);
+    const wasteTypes = ref([]);
+    const wasteSelectValue = ref([]);
+    const modalIsOpen = ref(false)
+
+
     const photo = ref(null);
+    const gatheredWastes = ref([]);
 
     function getAllSortPoints() {
       return baseApi.get(reportUrls.getAllSortPointsName).then((result) => {
@@ -323,7 +283,31 @@ export default {
       photo.value = event.target.files[0].name;
       //this.imageUrl = URL.createObjectURL(file);
     }
-
+    function getWasteByPk(pk) {
+      return wasteTypes.value.find((key) => key.pk === pk);
+    }
+    function getWasteSrcImageByPk(pk) {
+      const nameStatus = getWasteByPk(pk)?.name;
+      if (nameStatus) {
+        return require(`@/${wasteDictImagesDict[nameStatus]}`);
+      }
+      return "";
+    }
+    function getWasteTypesDict() {
+      baseApi.get("/review/statuses-waste-types-dict/").then((response) => {
+        wasteTypes.value = wasteTypes.value.concat(response.data.results);
+      });
+    }
+    function changeWasteInputHandler(event, item) {
+        gatheredWastes.value[item] = event.target.value
+    }
+    function openModal() {
+      modalIsOpen.value = true;
+    }
+    function closeModal() {
+      modalIsOpen.value = false;
+    }
+    
     const nameTrashPlaceSelectData = ref("place");
 
     const trashDictSelect = {
@@ -337,18 +321,26 @@ export default {
     }
 
     function deleteFile() {
-      fileInput.value.value = '';
-      photo.value = '';
+      fileInput.value.value = "";
+      photo.value = "";
     }
 
     function setDataByTrashPlace(name) {
       nameTrashPlaceSelectData.value = name;
     }
 
+    function deleteWaste(item) {
+      delete gatheredWastes[item];
+      const indArr = wasteSelectValue.value.indexOf(item);
+      const lenArr = wasteSelectValue.value.length;
+      wasteSelectValue.value = [...wasteSelectValue.value.slice(0, indArr), ...wasteSelectValue.value.slice(indArr + 1, lenArr)];
+    }
+
     getAllEvents();
     getAllRoutes();
     getAllPlaces();
     getAllSortPoints();
+    getWasteTypesDict();
 
     return {
       getDataByTrashPlace,
@@ -360,6 +352,16 @@ export default {
       handleFileChange,
       photo,
       deleteFile,
+      getWasteSrcImageByPk,
+      wasteTypes,
+      wasteSelectValue,
+      getWasteByPk,
+      gatheredWastes,
+      changeWasteInputHandler,
+      deleteWaste,
+      modalIsOpen,
+      openModal,
+      closeModal,
     };
   },
 };
